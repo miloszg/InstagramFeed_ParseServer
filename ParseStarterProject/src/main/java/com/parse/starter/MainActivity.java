e15ecd99d039029be08dc0cca82da01f9df34396
Miłosz <milosz.gus@gmail.com>
2019-02-15 17:19:18 +0100

    aktywnosc wyswietlenia uzytkownikow i pelna funkcjonalnosc logowania uzytkownika

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -11,9 +11,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
+import android.widget.TextClock;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
@@ -34,28 +41,81 @@ import java.util.List;
 import static com.google.android.gms.analytics.internal.zzy.v;
 
 
-public class MainActivity extends AppCompatActivity {
-    public void signUpClicked(View view){
-        EditText usernameEditText= (EditText) findViewById(R.id.usernameEditText);
-        EditText passwordEditText= (EditText) findViewById(R.id.passwordEditText);
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,View.OnKeyListener {
+    Boolean signupModeActive = true;
+    TextView loginTextView;
+    EditText passwordEditText;
+    EditText usernameEditText;
 
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction()==KeyEvent.ACTION_DOWN){
+            signUpClicked(v);
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.loginTextView){
+            Button signUpButton=findViewById(R.id.signUpButton);
+
+            if(signupModeActive) {
+                signupModeActive=false;
+                signUpButton.setText("Login");
+                loginTextView.setText("or, Sign Up");
+            } else {
+                signupModeActive=true;
+                signUpButton.setText("Sign Up");
+                loginTextView.setText("or, Login");
+            }
+
+        } else if(view.getId()==R.id.logoImage || view.getId()==R.id.backgroundLayout) {
+            InputMethodManager inputMethodManager= (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+
+        }
+    }
+
+    public void showUserList(){
+        Intent intent=new Intent(getApplicationContext(),UserListActivity.class);
+        startActivity(intent);
+    }
+
+    public void signUpClicked(View view){
         if(usernameEditText.getText().toString().matches("") || passwordEditText.getText().toString().matches("")){
             Toast.makeText(this, "Username and password are required", Toast.LENGTH_SHORT).show();
         } else {
-            ParseUser user =new ParseUser();
-            user.setUsername(usernameEditText.getText().toString());
-            user.setPassword(passwordEditText.getText().toString());
+            if(signupModeActive) {
+                ParseUser user = new ParseUser();
+                user.setUsername(usernameEditText.getText().toString());
+                user.setPassword(passwordEditText.getText().toString());
 
-            user.signUpInBackground(new SignUpCallback() {
-                @Override
-                public void done(ParseException e) {
-                    if(e==null){
-                        Log.i("Sign-up","Success");
-                    } else {
-                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Log.i("Sign-up", "Success");
+                            showUserList();
+                        } else {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
                     }
-                }
-            });
+                });
+            } else {
+                //LOGIN
+                ParseUser.logInInBackground(usernameEditText.getText().toString(), passwordEditText.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser parseUser, ParseException e) {
+                        if(parseUser!=null){
+                            Log.i("Login","Done");
+                        } else {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
         }
     }
 
@@ -65,7 +125,19 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    loginTextView=findViewById(R.id.loginTextView);
+    loginTextView.setOnClickListener(this);
+    usernameEditText= findViewById(R.id.usernameEditText);
+    passwordEditText= findViewById(R.id.passwordEditText);
+    ImageView logoImage=findViewById(R.id.logoImage);
+    RelativeLayout layout=findViewById(R.id.backgroundLayout);
+    logoImage.setOnClickListener(this);
+    layout.setOnClickListener(this);
 
+    passwordEditText.setOnKeyListener(this);
+    if(ParseUser.getCurrentUser()!=null){
+        showUserList();
+    }
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
